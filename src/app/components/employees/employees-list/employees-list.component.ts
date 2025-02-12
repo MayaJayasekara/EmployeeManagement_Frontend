93d92d7114695fd50503589b22fcd7dbc2fc21e9
MayaJayasekara <mokshalamjayasekara10@gmail.com>
2025-02-12 15:25:12 +0530

    create employee

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -11,7 +11,28 @@ export class EmployeesListComponent implements OnInit {
 
   employees: Employee[] = [
     {
-     name: ''
+      id: 'sb4ed4cc-f31b-44b-a06e-05ce7b322892',
+      name: 'John Doe',
+      email: 'Jhon.doe@email.com',
+      phone: 998877665,
+      salary: 6000,
+      department:'Human Reources'
+    },
+    {
+      id: 'c705cd8b-0297-441c-af5b-102620816a70',
+      name: 'Sameer Saini',
+      email: 'sameer.saini@email.com',
+      phone: 789789789,
+      salary: 7000,
+      department:'Information Tecnology'
+    },
+    {
+      id: 'sb4ed4cc-f31b-44b-a06e-05ce7b322892',
+      name: 'John Doe',
+      email: 'Jhon.doe@email.com',
+      phone: 998877665,
+      salary: 6000,
+      department:'Human Reources'
     }
   ];
   constructor() { }
